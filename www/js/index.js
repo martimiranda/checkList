8732dec5b851599b003288e5c0575a5342ba54a6
martimiranda <martimiranda2356@gmail.com>
2024-01-17 18:33:32 +0100

    persistencia

--- www/js/index.js
+++ www/js/index.js
@@ -22,6 +22,9 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+    refreshTasks();
+    //guardarLI();
+    //mostrarLI();
     // Cordova is now initialized. Have fun!
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
@@ -29,6 +32,11 @@ function onDeviceReady() {
     $('#homePage').on('click', '.borrar', function() {
         var elementoLi = $(this).closest('li');
         elementoLi.remove();
+        var id = elementoLi.attr('id');
+        var arrayDades = localStorage.getItem('dades').split(',');
+        var numero = parseInt(id.replace(/\D/g, ''), 10);
+        arrayDades.splice(numero-1, numero-1);
+        localStorage.setItem('dades',arrayDades);
         $('#homePage ul[data-role="listview"]').listview('refresh');
     });
     
@@ -38,8 +46,10 @@ function onDeviceReady() {
         // Coloca aquí el código que deseas ejecutar cuando se hace clic en el botón
         
     var newListItem = $('<li><a href="#"><button class="borrar">Borrar</button> <button class="editar">Editar</button><input class="escritura" style="display:none;"></input><button class="aceptar" style="display:none;">Aceptar</button>'+task+'</a></li>');
-    
-    
+    var arrayDades = localStorage.getItem('dades').split(',');
+    arrayDades.push(task);
+    localStorage.setItem('dades',arrayDades);
+
 
 
     // Agregar el nuevo elemento al listview
@@ -60,8 +70,43 @@ function onDeviceReady() {
     $('#homePage').on('click', '.aceptar', function() {
         var elementoLi = $(this).closest('li');
         var text = elementoLi.find('.escritura').val();
+        var id = elementoLi.attr('id');
         elementoLi.find('a').contents().last()[0].nodeValue = text;
         elementoLi.find('.escritura').css('display', 'none');
         elementoLi.find('.aceptar').css('display', 'none');
+        var arrayDades = localStorage.getItem('dades').split(',');
+        var numero = parseInt(id.replace(/\D/g, ''), 10);
+       // console.log(numero);
+        arrayDades[numero-1]=text;
+        localStorage.setItem('dades',arrayDades);
+
+console.log(miArray);
+
     });
+}
+function guardarLI(){
+    var tasks = $('#tasks li').outerHTML.toArray();
+    console.log(tasks);
+    localStorage.setItem('dades',JSON.stringify(tasks));
+}
+function LI(){
+    var tasks = JSON.parse(localStorage.getItem('dades'));
+    for(task of tasks){
+        $('#task').append(task);
+    }
+}
+
+function refreshTasks(){
+    var arrayDades = localStorage.getItem('dades').split(',');
+    var i =0;
+    for(dada of arrayDades){
+        i++;
+        console.log(dada);
+        var newListItem = $('<li><a href="#"><button class="borrar">Borrar</button> <button class="editar">Editar</button><input class="escritura" style="display:none;"></input><button class="aceptar" style="display:none;">Aceptar</button>'+dada+'</a></li>');
+        newListItem.attr('id', 'task' + i);
+        // Agregar el nuevo elemento al listview
+        $('#homePage ul[data-role="listview"]').append(newListItem);
+    }
+    // Refrescar el diseño de jQuery Mobile después de agregar el nuevo elemento
+    $('#homePage ul[data-role="listview"]').listview('refresh');
 }
